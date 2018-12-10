6276bf149da2a6d1066bc447b1b830a1887b7109
anajosep <anand.francis.joseph.augustin@oracle.com>
2018-12-10 01:42:39 -0800

    Classpath changes for including jakarta.ws.rs-api.jar and fixed JAXR standalone TCK failures.
    Signed-off-by: anajosep <anand.francis.joseph.augustin@oracle.com>

--- docker/jaxrstck.sh
+++ docker/jaxrstck.sh
@@ -51,7 +51,7 @@ sed -i "s#^work.dir=.*#work.dir=$TCK_HOME/jaxrstckwork/jaxrstck#g" ts.jte
 sed -i "s#^impl.vi=.*#impl.vi=glassfish#g" ts.jte
 sed -i 's#jaxrs_impl\.classes=.*#jaxrs_impl.classes=${web.home}/modules/jakarta.json.jar:${web.home}/modules/jakarta.json.bind-api.jar:${web.home}/modules/jakarta.json.jar:${web.home}/modules/jsonp-jaxrs.jar:${web.home}/modules/jersey-client.jar:${web.home}/modules/jersey-common.jar:${web.home}/modules/jersey-server.jar:${web.home}/modules/jersey-container-servlet.jar:${web.home}/modules/jersey-container-servlet-core.jar:${web.home}/modules/jersey-media-jaxb.jar:${web.home}/modules/jersey-media-sse.jar:${web.home}/modules/jersey-hk2.jar:${web.home}/modules/osgi-resource-ocator.jar:${web.home}/modules/jakarta.inject.jar:${web.home}/modules/guava.jar:${web.home}/modules/hk2-api.jar:${web.home}/modules/hk2-locator.jar:${web.home}/modules/hk2-utils.jar:${web.home}/modules/cglib.jar:${web.home}/modules/asm-all-repackaged.jar:${web.home}/modules/bean-validator.jar:${web.home}/modules/endorsed/jakarta.annotation-api.jar#g' ts.jte
 sed -i 's#jaxrs_impl_lib=.*#jaxrs_impl_lib=${web.home}/modules/jersey-container-servlet-core.jar#g' ts.jte
-sed -i 's#jaxrs\.classes=.*#jaxrs.classes=${web.home}/modules/javax.ws.rs-api.jar#g' ts.jte
+sed -i 's#jaxrs\.classes=.*#jaxrs.classes=${web.home}/modules/jakarta.ws.rs-api.jar#g' ts.jte
 sed -i 's#servlet_adaptor=.*#servlet_adaptor=org\/glassfish\/jersey\/servlet\/ServletContainer.class#g' ts.jte
 sed -i 's#servlet.classes=.*#servlet.classes=${web.home}/modules/jakarta.servlet-api.jar#g' ts.jte
 sed -i 's#jaxrs_impl_name=.*#jaxrs_impl_name=jersey#g' ts.jte
