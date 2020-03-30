eaaebde4f6f1c9dd104dad7cdf7f9c25ef3bd64f
Tomas Kraus <Tomas.Kraus@oracle.com>
2020-03-30 15:02:09 +0200

    Update javax to jakarta for jakarta.xml.soap and jakarta.activation.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- src/com/sun/ts/tests/saaj/ee/Standalone/Client.java
+++ src/com/sun/ts/tests/saaj/ee/Standalone/Client.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -25,8 +25,8 @@ import com.sun.ts.lib.porting.*;
 import com.sun.ts.lib.harness.*;
 import com.sun.ts.tests.saaj.common.*;
 
-import javax.xml.soap.*;
-import javax.activation.*;
+import jakarta.xml.soap.*;
+import jakarta.activation.*;
 
 import java.io.*;
 import java.net.*;
