24332efadbebac664f63fabfb50eac370885a912
RohitKumarJain <rohit.ku.jain@oracle.com>
2020-04-14 14:56:23 +0000

    Renaming javax.validation->jakarta.validation...
    Signed-off-by: RohitKumarJain <rohit.ku.jain@oracle.com>

--- src/com/sun/ts/tests/jaxrs/platform/beanvalidation/annotation/ConstraintDefinitionValidator.java
+++ src/com/sun/ts/tests/jaxrs/platform/beanvalidation/annotation/ConstraintDefinitionValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -16,8 +16,8 @@
 
 package com.sun.ts.tests.jaxrs.platform.beanvalidation.annotation;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
 import com.sun.ts.tests.jaxrs.common.provider.StringBean;
 
