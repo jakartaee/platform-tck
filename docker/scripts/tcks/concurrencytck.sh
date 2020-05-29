1c1a275e0865581271fee1761c19fb92e05cf9d6
RohitKumarJain <rohit.ku.jain@oracle.com>
2020-05-29 08:03:55 +0000

    covering pending javax* to jakarta* conversion.
    Signed-off-by: RohitKumarJain <rohit.ku.jain@oracle.com>

--- docker/scripts/tcks/concurrencytck.sh
+++ docker/scripts/tcks/concurrencytck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 #
-# Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2020 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v. 2.0, which is available at
@@ -34,7 +34,7 @@ rm -f $TS_HOME/dist/com/sun/ts/tests/concurrency/spec/ContextService/contextProp
 
 cd $TS_HOME/bin
 sed -i "s#^webcontainer\.home=.*#webcontainer.home=$TCK_HOME/glassfish5/glassfish#g" ts.jte
-sed -i 's#^concurrency\.classes=.*#concurrency.classes=${webcontainer.home}/modules/javax.enterprise.concurrent-api.jar${pathsep}${webcontainer.home}/modules/javax.servlet-api.jar${pathsep}${webcontainer.home}/modules/javax.ejb-api.jar${pathsep}${webcontainer.home}/modules/jta.jar${pathsep}${webcontainer.home}/modules/javax.enterprise.deploy-api.jar#g' ts.jte
+sed -i 's#^concurrency\.classes=.*#concurrency.classes=${webcontainer.home}/modules/javax.enterprise.concurrent-api.jar${pathsep}${webcontainer.home}/modules/javax.servlet-api.jar${pathsep}${webcontainer.home}/modules/jakarta.ejb-api.jar${pathsep}${webcontainer.home}/modules/jta.jar${pathsep}${webcontainer.home}/modules/javax.enterprise.deploy-api.jar#g' ts.jte
 sed -i "s#^report.dir=.*#report.dir=$TCK_HOME/concurrencytckreport#g" ts.jte
 sed -i "s#^work.dir=.*#work.dir=$TCK_HOME/concurrencytckwork#g" ts.jte
 
