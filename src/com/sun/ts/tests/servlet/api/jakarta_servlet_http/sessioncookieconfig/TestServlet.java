d7f95ea4c12a118f6e4fa7005abe7fb2e117d2c4
Mark Thomas <markt@apache.org>
2021-11-22 21:43:51 +0000

    Fix the SessionCookieConfig tests now comment is unused
    The test used comment to pass data. Comment is now a NO-OP so it can't
    be used going forwards. The use of comment has been replaced with a
    ServletContext attribute.

--- src/com/sun/ts/tests/servlet/api/jakarta_servlet_http/sessioncookieconfig/TestServlet.java
+++ src/com/sun/ts/tests/servlet/api/jakarta_servlet_http/sessioncookieconfig/TestServlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -36,7 +36,7 @@ public class TestServlet extends HttpTCKServlet {
       HttpServletResponse response) throws ServletException, IOException {
     request.getSession(true);
 
-    String results = getServletContext().getSessionCookieConfig().getComment();
+    String results = (String) getServletContext().getAttribute(TestListener.class.getName());
 
     if (results.indexOf("-FAILED-") > -1) {
       ServletTestUtil.printResult(
