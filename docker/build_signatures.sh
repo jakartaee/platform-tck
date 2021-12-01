7ea4c5ca30239287003915331da8ff6c5d838055
Scott Marlow <smarlow@redhat.com>
2021-12-01 10:25:53 -0500

    more signature file name changes
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- docker/build_signatures.sh
+++ docker/build_signatures.sh
@@ -100,60 +100,58 @@ export CLASSNAME="com.sun.tdk.signaturetest.Main"
 export CLASSPATH=$CLASSPATH:${TS_HOME}/lib/sigtest.jar
 export SIGFILEPATH=$TS_HOME/src/com/sun/ts/tests/signaturetest/signature-repository
 
-# jakarta.persistence
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.persistence -FileName ${SIGFILEPATH}/jakarta.persistence.sig_3.0_se11
 # jakarta.annotation
-java $jdk9options $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0 -package jakarta.annotation -FileName ${SIGFILEPATH}/jakarta.annotation.sig_2.0_se11
+java $jdk9options $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0 -package jakarta.annotation -FileName ${SIGFILEPATH}/jakarta.annotation.sig_2.1_se11
 # jakarta.security.jacc
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.security.jacc -FileName ${SIGFILEPATH}/jakarta.security.jacc.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.security.jacc -FileName ${SIGFILEPATH}/jakarta.security.jacc.sig_3.0_se11
 # jakarta.batch
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.batch -FileName ${SIGFILEPATH}/jakarta.batch.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.batch -FileName ${SIGFILEPATH}/jakarta.batch.sig_2.1_se11
 # jakarta.decorator
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.decorator -FileName ${SIGFILEPATH}/jakarta.decorator.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.decorator -FileName ${SIGFILEPATH}/jakarta.decorator.sig_4.0_se11
 # jakarta.ejb
 java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.ejb -FileName ${SIGFILEPATH}/jakarta.ejb.sig_4.0_se11
 # jakarta.el
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.el -FileName ${SIGFILEPATH}/jakarta.el.sig_4.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.el -FileName ${SIGFILEPATH}/jakarta.el.sig_5.0_se11
 # jakarta.enterprise
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.enterprise.concurrent -package jakarta.enterprise -FileName ${SIGFILEPATH}/jakarta.enterprise.sig_3.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.enterprise.concurrent -package jakarta.enterprise -FileName ${SIGFILEPATH}/jakarta.enterprise.sig_4.0_se11
 # jakarta.enterprise.concurrent
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.enterprise.concurrent -FileName ${SIGFILEPATH}/jakarta.enterprise.concurrent.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.enterprise.concurrent -FileName ${SIGFILEPATH}/jakarta.enterprise.concurrent.sig_3.0_se11
 # jakarta.faces
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.faces -FileName ${SIGFILEPATH}/jakarta.faces.sig_3.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.faces -FileName ${SIGFILEPATH}/jakarta.faces.sig_4.0_se11
 # jakarta.inject
 java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.inject -FileName ${SIGFILEPATH}/jakarta.inject.sig_2.0_se11
 # jakarta.interceptor
 java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.interceptor -FileName ${SIGFILEPATH}/jakarta.interceptor.sig_2.0_se11
 # jakarta.jms
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.jms -FileName ${SIGFILEPATH}/jakarta.jms.sig_3.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.jms -FileName ${SIGFILEPATH}/jakarta.jms.sig_3.1_se11
 # jakarta.json
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.json.bind -package jakarta.json -FileName ${SIGFILEPATH}/jakarta.json.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.json.bind -package jakarta.json -FileName ${SIGFILEPATH}/jakarta.json.sig_2.1_se11
 # jakarta.json.bind
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.json.bind -FileName ${SIGFILEPATH}/jakarta.json.bind.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.json.bind -FileName ${SIGFILEPATH}/jakarta.json.bind.sig_2.1_se11
 # jakarta.mail
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.mail -FileName ${SIGFILEPATH}/jakarta.mail.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.mail -FileName ${SIGFILEPATH}/jakarta.mail.sig_2.1_se11
 # jakarta.persistence
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.persistence -FileName ${SIGFILEPATH}/jakarta.persistence.sig_3.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.persistence -FileName ${SIGFILEPATH}/jakarta.persistence.sig_3.1_se11
 # jakarta.resource
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.resource -FileName ${SIGFILEPATH}/jakarta.resource.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.resource -FileName ${SIGFILEPATH}/jakarta.resource.sig_2.1_se11
 # jakarta.security.enterprise
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.security.enterprise -FileName ${SIGFILEPATH}/jakarta.security.enterprise.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.security.enterprise -FileName ${SIGFILEPATH}/jakarta.security.enterprise.sig_3.0_se11
 # jakarta.security.auth.message
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.security.auth.message -FileName ${SIGFILEPATH}/jakarta.security.auth.message.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.security.auth.message -FileName ${SIGFILEPATH}/jakarta.security.auth.message.sig_3.0_se11
 # jakarta.servlet (exclude jakarta.servlet.jsp)
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.servlet.jsp -package jakarta.servlet -FileName ${SIGFILEPATH}/jakarta.servlet.sig_5.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.servlet.jsp -package jakarta.servlet -FileName ${SIGFILEPATH}/jakarta.servlet.sig_6.0_se11
 # jakarta.servlet.jsp (exclude jakarta.servlet.jsp.jstl)
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.servlet.jsp.jstl -package jakarta.servlet.jsp -FileName ${SIGFILEPATH}/jakarta.servlet.jsp.sig_3.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -Exclude jakarta.servlet.jsp.jstl -package jakarta.servlet.jsp -FileName ${SIGFILEPATH}/jakarta.servlet.jsp.sig_3.1_se11
 # jakarta.servlet.jsp.jstl
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.servlet.jsp.jstl -FileName ${SIGFILEPATH}/jakarta.servlet.jsp.jstl.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.servlet.jsp.jstl -FileName ${SIGFILEPATH}/jakarta.servlet.jsp.jstl.sig_3.0_se11
 # jakarta.transaction
 java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.transaction -FileName ${SIGFILEPATH}/jakarta.transaction.sig_2.0_se11
 # jakarta.validation
 java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.validation -FileName ${SIGFILEPATH}/jakarta.validation.sig_3.0_se11
 # jakarta.websocket
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.websocket -FileName ${SIGFILEPATH}/jakarta.websocket.sig_2.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.websocket -FileName ${SIGFILEPATH}/jakarta.websocket.sig_2.1_se11
 # jakarta.ws.rs
-java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.ws.rs -FileName ${SIGFILEPATH}/jakarta.ws.rs.sig_3.0_se11
+java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.ws.rs -FileName ${SIGFILEPATH}/jakarta.ws.rs.sig_3.1_se11
 # jakarta.xml.ws
 java $CLASSNAME Setup ${OPTIONS} -classpath ${sigTestClasspath} -apiVersion 1.0  -package jakarta.xml.ws -FileName ${SIGFILEPATH}/jakarta.xml.ws.sig_3.0_se11
 # jakarta.xml.soap
