2a130208da0e133716627a0815f7fadc8a0b9d28
gurunrao <gurunandan.rao@oracle.com>
2022-03-16 13:58:03 +0530

    Remove -Djava.locale.providers=COMPAT for platform tck runs.
    Signed-off-by: gurunrao <gurunandan.rao@oracle.com>

--- docker/run_jakartaeetck.sh
+++ docker/run_jakartaeetck.sh
@@ -253,10 +253,6 @@ ${CTS_HOME}/vi/$GF_VI_TOPLEVEL_DIR/glassfish/bin/asadmin --user admin --password
 ${CTS_HOME}/vi/$GF_VI_TOPLEVEL_DIR/glassfish/bin/asadmin --user admin --passwordfile ${ADMIN_PASSWORD_FILE} start-domain
 ${CTS_HOME}/vi/$GF_VI_TOPLEVEL_DIR/glassfish/bin/asadmin --user admin --passwordfile ${ADMIN_PASSWORD_FILE} version
 ${CTS_HOME}/vi/$GF_VI_TOPLEVEL_DIR/glassfish/bin/asadmin --user admin --passwordfile ${ADMIN_PASSWORD_FILE} create-jvm-options -Djava.security.manager
-#https://github.com/eclipse-ee4j/jakartaee-tck/issues/631
-if [[ $test_suite == jstl ]]; then
-  ${CTS_HOME}/vi/$GF_VI_TOPLEVEL_DIR/glassfish/bin/asadmin --user admin --passwordfile ${ADMIN_PASSWORD_FILE} create-jvm-options -Djava.locale.providers=COMPAT
-fi
 ${CTS_HOME}/vi/$GF_VI_TOPLEVEL_DIR/glassfish/bin/asadmin --user admin --passwordfile ${ADMIN_PASSWORD_FILE} stop-domain
 
 sleep 5
