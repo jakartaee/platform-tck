1cf834c3018fe81e4e6dc0a103fc20cdcab5f192
Scott Marlow <smarlow@redhat.com>
2022-05-23 10:29:04 -0400

    update for 1007, move ejb30.lite.packaging.war.webinflibonly.JsfClient to new package jsfwebinflibonl
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- src/com/sun/ts/tests/ejb30/lite/packaging/war/jsfwebinflibonl/JsfClient.java
+++ src/com/sun/ts/tests/ejb30/lite/packaging/war/jsfwebinflibonl/JsfClient.java
@@ -17,7 +17,7 @@
 /*
  * $Id$
  */
-package com.sun.ts.tests.ejb30.lite.packaging.war.webinflibonly;
+package com.sun.ts.tests.ejb30.lite.packaging.war.jsfwebinflibonl.webinflibonly;
 
 /**
  * See com.sun.ts.tests.ejb30.lite.packaging.war.webinflib.Client. All ejb
