78465de96371841fbf72d5cde572c63e0e255b73
Alwin Joseph <alwin.joseph@oracle.com>
2023-04-17 16:39:26 +0530

    moving methodreference test from src/ folder

--- el/src/main/java/com/sun/ts/tests/el/api/jakarta_el/methodreference/ELClientIT.java
+++ el/src/main/java/com/sun/ts/tests/el/api/jakarta_el/methodreference/ELClientIT.java
@@ -17,7 +17,6 @@ package com.sun.ts.tests.el.api.jakarta_el.methodreference;
 
 import java.util.Properties;
 
-import com.sun.javatest.Status;
 import com.sun.ts.lib.harness.ServiceEETest;
 import com.sun.ts.lib.util.TestUtil;
 import com.sun.ts.tests.common.el.api.expression.ExpressionTest;
@@ -34,24 +33,45 @@ import jakarta.el.MethodReference;
 import jakarta.el.PropertyNotFoundException;
 import jakarta.el.ValueExpression;
 
-public class ELClient extends ServiceEETest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+public class ELClientIT extends ServiceEETest {
 
   private static final String NL = System.getProperty("line.seperator", "\n");
 
   private Properties testProps;
 
-  public static void main(String[] args) {
-    ELClient theTests = new ELClient();
-    Status s = theTests.run(args, System.out, System.err);
-    s.exit();
+  public ELClientIT(){
+    this.testProps = System.getProperties();
   }
 
-  public void setup(String[] args, Properties p) throws Fault {
-    TestUtil.logTrace("Setup method called");
-    this.testProps = p;
+  // public static void main(String[] args) {
+  //   ELClientIT theTests = new ELClientIT();
+  //   Status s = theTests.run(args, System.out, System.err);
+  //   s.exit();
+  // }
+
+  // public void setup(String[] args, Properties p) throws Fault {
+  //   TestUtil.logTrace("Setup method called");
+  //   this.testProps = p;
+  // }
+
+  @AfterEach
+  public void cleanup() throws Exception {
+    TestUtil.logTrace("Cleanup method called");
   }
 
-  public void cleanup() throws Fault {
+  @BeforeEach
+  void logStartTest(TestInfo testInfo) {
+    TestUtil.logMsg("STARTING TEST : "+testInfo.getDisplayName());
+  }
+
+  @AfterEach
+  void logFinishTest(TestInfo testInfo) {
+    TestUtil.logMsg("FINISHED TEST : "+testInfo.getDisplayName());
   }
 
   /**
@@ -63,6 +83,7 @@ public class ELClient extends ServiceEETest {
    *                 MethodReference.getAnnotations()
    *                 MethodReference.getEvaluatedParameters
    */
+  @Test
   public void methodReferenceTest() throws Fault {
 
     StringBuffer buf = new StringBuffer();
