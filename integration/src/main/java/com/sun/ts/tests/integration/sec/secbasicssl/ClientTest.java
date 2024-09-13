62ac61eb00a3521973af79cb8a4853c1bff41190
Scott M Stark <starksm64@gmail.com>
2024-09-13 08:17:57 -0600

    Migrate the integration tests Fixes #1479
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

---
+++ integration/src/main/java/com/sun/ts/tests/integration/sec/secbasicssl/ClientTest.java
@@ -0,0 +1,156 @@
+package com.sun.ts.tests.integration.sec.secbasicssl;
+
+import com.sun.ts.tests.integration.sec.secbasicssl.Client;
+import java.net.URL;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import tck.arquillian.porting.lib.spi.TestArchiveProcessor;
+import tck.arquillian.protocol.common.TargetVehicle;
+
+
+
+@ExtendWith(ArquillianExtension.class)
+@Tag("integration")
+@Tag("platform")
+@Tag("tck-javatest")
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class ClientTest extends com.sun.ts.tests.integration.sec.secbasicssl.Client {
+    /**
+        EE10 Deployment Descriptors:
+        integration_sec_secbasicssl: ear.sun-application.xml
+        integration_sec_secbasicssl_web: WEB-INF/web.xml
+
+        Found Descriptors:
+        War:
+
+        /com/sun/ts/tests/integration/sec/secbasicssl/integration_sec_secbasicssl_web.xml
+        Ear:
+
+        */
+        @TargetsContainer("tck-javatest")
+        @OverProtocol("javatest")
+        @Deployment(name = "integration_sec_secbasicssl", order = 2)
+        public static EnterpriseArchive createDeployment(@ArquillianResource TestArchiveProcessor archiveProcessor) {
+        // War
+            // the war with the correct archive name
+            WebArchive integration_sec_secbasicssl_web = ShrinkWrap.create(WebArchive.class, "integration_sec_secbasicssl_web.war");
+            // The web.xml descriptor
+            URL warResURL = Client.class.getResource("integration_sec_secbasicssl_web.xml");
+            integration_sec_secbasicssl_web.addAsWebInfResource(warResURL, "web.xml");
+            // The sun-web.xml descriptor
+            warResURL = Client.class.getResource("integration_sec_secbasicssl_web.war.sun-web.xml");
+            integration_sec_secbasicssl_web.addAsWebInfResource(warResURL, "sun-web.xml");
+
+            // Any libraries added to the war
+
+            // Web content
+            warResURL = Client.class.getResource("authorized.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/authorized.jsp");
+            warResURL = Client.class.getResource("basicSSL.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/basicSSL.jsp");
+            warResURL = Client.class.getResource("requestAttributes.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/requestAttributes.jsp");
+            warResURL = Client.class.getResource("rolereverse.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/rolereverse.jsp");
+            warResURL = Client.class.getResource("webApiRemoteUser1.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/webApiRemoteUser1.jsp");
+            warResURL = Client.class.getResource("/com/sun/ts/tests/integration/sec/secbasicssl/webNoAuthz.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/webNoAuthz.jsp");
+            warResURL = Client.class.getResource("webNotInRole.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/webNotInRole.jsp");
+            warResURL = Client.class.getResource("webRoleRefScope1.jsp");
+            integration_sec_secbasicssl_web.addAsWebResource(warResURL, "/webRoleRefScope1.jsp");
+
+           // Call the archive processor
+           archiveProcessor.processWebArchive(integration_sec_secbasicssl_web, Client.class, warResURL);
+
+
+        // Ear
+            EnterpriseArchive integration_sec_secbasicssl_ear = ShrinkWrap.create(EnterpriseArchive.class, "integration_sec_secbasicssl.ear");
+
+            // Any libraries added to the ear
+
+            // The component jars built by the package target
+            integration_sec_secbasicssl_ear.addAsModule(integration_sec_secbasicssl_web);
+
+
+
+            // The application.xml descriptor
+            URL earResURL = null;
+            // Call the archive processor
+            archiveProcessor.processEarArchive(integration_sec_secbasicssl_ear, Client.class, earResURL);
+        return integration_sec_secbasicssl_ear;
+        }
+
+        @Test
+        @Override
+        public void test_login_basic_over_ssl() throws java.lang.Exception {
+            super.test_login_basic_over_ssl();
+        }
+
+        @Test
+        @Override
+        public void test_web_no_authz() throws java.lang.Exception {
+            super.test_web_no_authz();
+        }
+
+        @Test
+        @Override
+        public void test_web_not_in_role() throws java.lang.Exception {
+            super.test_web_not_in_role();
+        }
+
+        @Test
+        @Override
+        public void test_web_api_remoteuser_1() throws java.lang.Exception {
+            super.test_web_api_remoteuser_1();
+        }
+
+        @Test
+        @Override
+        public void test_web_api_remoteuser_2() throws java.lang.Exception {
+            super.test_web_api_remoteuser_2();
+        }
+
+        @Test
+        @Override
+        public void test_web_roleref_scope_1() throws java.lang.Exception {
+            super.test_web_roleref_scope_1();
+        }
+
+        @Test
+        @Override
+        public void test_web_roleref_scope_2() throws java.lang.Exception {
+            super.test_web_roleref_scope_2();
+        }
+
+        @Test
+        @Override
+        public void test_web_is_authz() throws java.lang.Exception {
+            super.test_web_is_authz();
+        }
+
+        @Test
+        @Override
+        public void test_request_attributes() throws java.lang.Exception {
+            super.test_request_attributes();
+        }
+
+
+}
---
+++ integration/src/main/java/com/sun/ts/tests/integration/session/servletejbjdbc/ClientTest.java
@@ -0,0 +1,125 @@
+package com.sun.ts.tests.integration.session.servletejbjdbc;
+
+import com.sun.ts.tests.integration.session.servletejbjdbc.URLClient;
+import java.net.URL;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import tck.arquillian.porting.lib.spi.TestArchiveProcessor;
+import tck.arquillian.protocol.common.TargetVehicle;
+
+
+
+@ExtendWith(ArquillianExtension.class)
+@Tag("integration")
+@Tag("platform")
+@Tag("tck-javatest")
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class ClientTest extends com.sun.ts.tests.integration.session.servletejbjdbc.URLClient {
+    /**
+        EE10 Deployment Descriptors:
+        int_S_servletejbjdbc: 
+        int_S_servletejbjdbc_ejb: META-INF/ejb-jar.xml,jar.sun-ejb-jar.xml
+        int_S_servletejbjdbc_servlet_web: WEB-INF/web.xml,war.sun-web.xml
+
+        Found Descriptors:
+        Ejb:
+
+        /com/sun/ts/tests/integration/session/servletejbjdbc/int_S_servletejbjdbc_ejb.xml
+        /com/sun/ts/tests/integration/session/servletejbjdbc/int_S_servletejbjdbc_ejb.jar.sun-ejb-jar.xml
+        War:
+
+        /com/sun/ts/tests/integration/session/servletejbjdbc/int_S_servletejbjdbc_servlet_web.xml
+        /com/sun/ts/tests/integration/session/servletejbjdbc/int_S_servletejbjdbc_servlet_web.war.sun-web.xml
+        Ear:
+
+        */
+        @TargetsContainer("tck-javatest")
+        @OverProtocol("javatest")
+        @Deployment(name = "int_S_servletejbjdbc", order = 2)
+        public static EnterpriseArchive createDeployment(@ArquillianResource TestArchiveProcessor archiveProcessor) {
+        // War
+            // the war with the correct archive name
+            WebArchive int_S_servletejbjdbc_servlet_web = ShrinkWrap.create(WebArchive.class, "int_S_servletejbjdbc_servlet_web.war");
+            // The class files
+            int_S_servletejbjdbc_servlet_web.addClasses(
+            com.sun.ts.tests.integration.session.servletejbjdbc.ServletTest.class,
+            com.sun.ts.tests.integration.session.servletejbjdbc.Teller.class
+            );
+            // The web.xml descriptor
+            URL warResURL = URLClient.class.getResource("int_S_servletejbjdbc_servlet_web.xml");
+            int_S_servletejbjdbc_servlet_web.addAsWebInfResource(warResURL, "web.xml");
+            // The sun-web.xml descriptor
+            warResURL = URLClient.class.getResource("int_S_servletejbjdbc_servlet_web.war.sun-web.xml");
+            int_S_servletejbjdbc_servlet_web.addAsWebInfResource(warResURL, "sun-web.xml");
+
+            // Any libraries added to the war
+
+            // Web content
+            warResURL = URLClient.class.getResource("/com/sun/ts/tests/integration/session/servletejbjdbc/contentRoot/client.html");
+            if(warResURL != null) {
+              int_S_servletejbjdbc_servlet_web.addAsWebResource(warResURL, "/client.html");
+            }
+
+           // Call the archive processor
+           archiveProcessor.processWebArchive(int_S_servletejbjdbc_servlet_web, URLClient.class, warResURL);
+
+        // Ejb 1
+            // the jar with the correct archive name
+            JavaArchive int_S_servletejbjdbc_ejb = ShrinkWrap.create(JavaArchive.class, "int_S_servletejbjdbc_ejb.jar");
+            // The class files
+            int_S_servletejbjdbc_ejb.addClasses(
+                com.sun.ts.tests.integration.session.servletejbjdbc.Teller.class,
+                com.sun.ts.tests.integration.util.DBSupport.class,
+                com.sun.ts.tests.integration.session.servletejbjdbc.TellerBean.class,
+                com.sun.ts.tests.integration.util.Account.class
+            );
+            // The ejb-jar.xml descriptor
+            URL ejbResURL1 = URLClient.class.getResource("int_S_servletejbjdbc_ejb.xml");
+            int_S_servletejbjdbc_ejb.addAsManifestResource(ejbResURL1, "ejb-jar.xml");
+            // The sun-ejb-jar.xml file
+            ejbResURL1 = URLClient.class.getResource("int_S_servletejbjdbc_ejb.jar.sun-ejb-jar.xml");
+            int_S_servletejbjdbc_ejb.addAsManifestResource(ejbResURL1, "sun-ejb-jar.xml");
+            // Call the archive processor
+            archiveProcessor.processEjbArchive(int_S_servletejbjdbc_ejb, URLClient.class, ejbResURL1);
+
+
+        // Ear
+            EnterpriseArchive int_S_servletejbjdbc_ear = ShrinkWrap.create(EnterpriseArchive.class, "int_S_servletejbjdbc.ear");
+
+            // Any libraries added to the ear
+
+            // The component jars built by the package target
+            int_S_servletejbjdbc_ear.addAsModule(int_S_servletejbjdbc_ejb);
+            int_S_servletejbjdbc_ear.addAsModule(int_S_servletejbjdbc_servlet_web);
+
+            // The application.xml descriptor
+            URL earResURL = null;
+            // Call the archive processor
+            archiveProcessor.processEarArchive(int_S_servletejbjdbc_ear, URLClient.class, earResURL);
+        return int_S_servletejbjdbc_ear;
+        }
+
+        @Test
+        @Override
+        public void test1() throws java.lang.Exception {
+            super.test1();
+        }
+
+
+}
