474f72ef6eba100d4782999b11fb594e0b7cfca9
Alwin Joseph <alwin.joseph@oracle.com>
2024-10-10 13:29:24 +0530

    add more main class

--- jpa/platform-tests/src/main/java/ee/jakarta/tck/persistence/core/query/language/Client1AppmanagednotxTest.java
+++ jpa/platform-tests/src/main/java/ee/jakarta/tck/persistence/core/query/language/Client1AppmanagednotxTest.java
@@ -94,7 +94,9 @@ public class Client1AppmanagednotxTest extends ee.jakarta.tck.persistence.core.q
             com.sun.ts.tests.common.vehicle.ejb3share.EntityTransactionWrapper.class,
             com.sun.ts.lib.harness.EETest.SetupException.class,
             com.sun.ts.tests.common.vehicle.VehicleClient.class,
-            com.sun.ts.tests.common.vehicle.ejb3share.NoopTransactionWrapper.class
+            com.sun.ts.tests.common.vehicle.ejb3share.NoopTransactionWrapper.class,
+            Client1.class,
+            Client1AppmanagednotxTest.class
             ).addClasses(ee.jakarta.tck.persistence.common.schema30.Util.getSchema30classes());
             // The application-client.xml descriptor
             URL resURL = Client1.class.getResource("/com/sun/ts/tests/common/vehicle/appmanagedNoTx/appmanagedNoTx_vehicle_client.xml");
