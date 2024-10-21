eb2a847894f9c8387701ac9e7e8b88fe62213249
Arjan Tijms <arjan.tijms@omnifish.ee>
2024-10-21 17:46:40 +0200

    Add application.xml and correct ejb-jar.xml

--- ejb30/src/main/java/com/sun/ts/tests/ejb30/misc/nomethodbean/ClientTest.java
+++ ejb30/src/main/java/com/sun/ts/tests/ejb30/misc/nomethodbean/ClientTest.java
@@ -22,8 +22,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import tck.arquillian.porting.lib.spi.TestArchiveProcessor;
 import tck.arquillian.protocol.common.TargetVehicle;
 
-
-
 @ExtendWith(ArquillianExtension.class)
 @Tag("platform")
 @Tag("ejb_3x_remote_optional")
@@ -33,120 +31,92 @@ import tck.arquillian.protocol.common.TargetVehicle;
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class ClientTest extends com.sun.ts.tests.ejb30.misc.nomethodbean.Client {
     /**
-        EE10 Deployment Descriptors:
-        ejb3_misc_nomethodbean: 
-        ejb3_misc_nomethodbean_ejb: jar.sun-ejb-jar.xml
-        ejb3_misc_nomethodbean_web: 
-
-        Found Descriptors:
-        Ejb:
-
-        /com/sun/ts/tests/ejb30/misc/nomethodbean/ejb3_misc_nomethodbean_ejb.jar.sun-ejb-jar.xml
-        War:
-
-        Ear:
-
-        */
-        @TargetsContainer("tck-javatest")
-        @OverProtocol("javatest")
-        @Deployment(name = "ejb3_misc_nomethodbean", order = 2)
-        public static EnterpriseArchive createDeployment(@ArquillianResource TestArchiveProcessor archiveProcessor) {
+     * EE10 Deployment Descriptors: ejb3_misc_nomethodbean: ejb3_misc_nomethodbean_ejb: jar.sun-ejb-jar.xml
+     * ejb3_misc_nomethodbean_web:
+     *
+     * Found Descriptors: Ejb:
+     *
+     * /com/sun/ts/tests/ejb30/misc/nomethodbean/ejb3_misc_nomethodbean_ejb.jar.sun-ejb-jar.xml War:
+     *
+     * Ear:
+     *
+     */
+    @TargetsContainer("tck-javatest")
+    @OverProtocol("javatest")
+    @Deployment(name = "ejb3_misc_nomethodbean", order = 2)
+    public static EnterpriseArchive createDeployment(@ArquillianResource TestArchiveProcessor archiveProcessor) {
         // War
-            // the war with the correct archive name
-            WebArchive ejb3_misc_nomethodbean_web = ShrinkWrap.create(WebArchive.class, "ejb3_misc_nomethodbean_web.war");
-            // The class files
-            ejb3_misc_nomethodbean_web.addClasses(
+        // the war with the correct archive name
+        WebArchive ejb3_misc_nomethodbean_web = ShrinkWrap.create(WebArchive.class, "ejb3_misc_nomethodbean_web.war");
+
+        // The class files
+        ejb3_misc_nomethodbean_web.addClasses(
             com.sun.ts.tests.servlet.common.servlets.HttpTCKServlet.class,
-            com.sun.ts.tests.ejb30.misc.nomethodbean.TestServlet.class,
-            com.sun.ts.tests.servlet.common.util.Data.class
-            );
-            // The web.xml descriptor
-            URL warResURL = Client.class.getResource("com/sun/ts/tests/ejb30/misc/nomethodbean/ejb3_misc_nomethodbean_web.xml");
-            if(warResURL != null) {
-              ejb3_misc_nomethodbean_web.addAsWebInfResource(warResURL, "web.xml");
-            }
-            // The sun-web.xml descriptor
-            warResURL = Client.class.getResource("/com/sun/ts/tests/ejb30/misc/nomethodbean/ejb3_misc_nomethodbean_web.war.sun-web.xml");
-            if(warResURL != null) {
-              ejb3_misc_nomethodbean_web.addAsWebInfResource(warResURL, "sun-web.xml");
-            }
+            com.sun.ts.tests.ejb30.misc.nomethodbean.TestServlet.class, com.sun.ts.tests.servlet.common.util.Data.class);
 
-            // Any libraries added to the war
+        // Call the archive processor
+        archiveProcessor.processWebArchive(ejb3_misc_nomethodbean_web, Client.class, null);
 
-            // Web content
-           // Call the archive processor
-           archiveProcessor.processWebArchive(ejb3_misc_nomethodbean_web, Client.class, warResURL);
 
         // Ejb
-            // the jar with the correct archive name
-            JavaArchive ejb3_misc_nomethodbean_ejb = ShrinkWrap.create(JavaArchive.class, "ejb3_misc_nomethodbean_ejb.jar");
-            // The class files
-            ejb3_misc_nomethodbean_ejb.addClasses(
-                com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodStatelessBean.class,
-                com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodStatefulBean.class
-            );
-            // The ejb-jar.xml descriptor
-            URL ejbResURL = Client.class.getResource("/com/sun/ts/tests/ejb30/misc/nomethodbean/ejb3_misc_nomethodbean_ejb.xml");
-            if(ejbResURL != null) {
-              ejb3_misc_nomethodbean_ejb.addAsManifestResource(ejbResURL, "ejb-jar.xml");
-            }
-            // The sun-ejb-jar.xml file
-            ejbResURL = Client.class.getResource("/com/sun/ts/tests/ejb30/misc/nomethodbean/ejb3_misc_nomethodbean_ejb.jar.sun-ejb-jar.xml");
-            if(ejbResURL != null) {
-              ejb3_misc_nomethodbean_ejb.addAsManifestResource(ejbResURL, "sun-ejb-jar.xml");
-            }
-            // Call the archive processor
-            archiveProcessor.processEjbArchive(ejb3_misc_nomethodbean_ejb, Client.class, ejbResURL);
+        // the jar with the correct archive name
+        JavaArchive ejb3_misc_nomethodbean_ejb = ShrinkWrap.create(JavaArchive.class, "ejb3_misc_nomethodbean_ejb.jar");
+
+        // The class files
+        ejb3_misc_nomethodbean_ejb.addClasses(
+            com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodStatelessBean.class,
+            com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodStatefulBean.class);
+
+        // The sun-ejb-jar.xml file
+        URL ejbResURL = Client.class.getResource("/com/sun/ts/tests/ejb30/misc/nomethodbean/ejb3_misc_nomethodbean_ejb.jar.sun-ejb-jar.xml");
+        if (ejbResURL != null) {
+            ejb3_misc_nomethodbean_ejb.addAsManifestResource(ejbResURL, "sun-ejb-jar.xml");
+        }
+
+        // Call the archive processor
+        archiveProcessor.processEjbArchive(ejb3_misc_nomethodbean_ejb, Client.class, ejbResURL);
+
 
         // Ear
-            EnterpriseArchive ejb3_misc_nomethodbean_ear = ShrinkWrap.create(EnterpriseArchive.class, "ejb3_misc_nomethodbean.ear");
+        EnterpriseArchive ejb3_misc_nomethodbean_ear = ShrinkWrap.create(EnterpriseArchive.class, "ejb3_misc_nomethodbean.ear");
 
-            // Any libraries added to the ear
-                URL libURL;
-                JavaArchive shared_lib = ShrinkWrap.create(JavaArchive.class, "shared.jar");
-                    // The class files
-                    shared_lib.addClasses(
-                        com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodLocalIF.class,
-                        com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodRemoteIF.class,
-                        com.sun.ts.tests.ejb30.common.helper.ServiceLocator.class
-                    );
+        // Any libraries added to the ear
+        JavaArchive shared_lib = ShrinkWrap.create(JavaArchive.class, "shared.jar");
 
+        // The class files
+        shared_lib.addClasses(
+            com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodLocalIF.class,
+            com.sun.ts.tests.ejb30.misc.nomethodbean.NoMethodRemoteIF.class,
+            com.sun.ts.tests.ejb30.common.helper.ServiceLocator.class);
 
-                ejb3_misc_nomethodbean_ear.addAsLibrary(shared_lib);
+        ejb3_misc_nomethodbean_ear.addAsLibrary(shared_lib);
 
+        // The component jars built by the package target
+        ejb3_misc_nomethodbean_ear.addAsModule(ejb3_misc_nomethodbean_ejb);
+        ejb3_misc_nomethodbean_ear.addAsModule(ejb3_misc_nomethodbean_web);
 
-            // The component jars built by the package target
-            ejb3_misc_nomethodbean_ear.addAsModule(ejb3_misc_nomethodbean_ejb);
-            ejb3_misc_nomethodbean_ear.addAsModule(ejb3_misc_nomethodbean_web);
+        // The application.xml descriptor
+        URL earResURL = Client.class.getResource("/com/sun/ts/tests/ejb30/misc/nomethodbean/application.xml");
+        if (earResURL != null) {
+            ejb3_misc_nomethodbean_ear.addAsManifestResource(earResURL, "application.xml");
+        }
 
+        // Call the archive processor
+        archiveProcessor.processEarArchive(ejb3_misc_nomethodbean_ear, Client.class, earResURL);
 
-
-            // The application.xml descriptor
-            URL earResURL = Client.class.getResource("/com/sun/ts/tests/ejb30/misc/nomethodbean/");
-            if(earResURL != null) {
-              ejb3_misc_nomethodbean_ear.addAsManifestResource(earResURL, "application.xml");
-            }
-            // The sun-application.xml descriptor
-            earResURL = Client.class.getResource("/com/sun/ts/tests/ejb30/misc/nomethodbean/.ear.sun-application.xml");
-            if(earResURL != null) {
-              ejb3_misc_nomethodbean_ear.addAsManifestResource(earResURL, "sun-application.xml");
-            }
-            // Call the archive processor
-            archiveProcessor.processEarArchive(ejb3_misc_nomethodbean_ear, Client.class, earResURL);
         return ejb3_misc_nomethodbean_ear;
-        }
+    }
 
-        @Test
-        @Override
-        public void noMethodStateless() throws java.lang.Exception {
-            super.noMethodStateless();
-        }
+    @Test
+    @Override
+    public void noMethodStateless() throws java.lang.Exception {
+        super.noMethodStateless();
+    }
 
-        @Test
-        @Override
-        public void noMethodStateful() throws java.lang.Exception {
-            super.noMethodStateful();
-        }
-
+    @Test
+    @Override
+    public void noMethodStateful() throws java.lang.Exception {
+        super.noMethodStateful();
+    }
 
 }
