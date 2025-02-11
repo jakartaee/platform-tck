ab44bcaebd5e1add5a18f16a4f7631d6e721d3ba
Scott M Stark <starksm64@gmail.com>
2025-02-11 21:54:29 +0000

    Skip non-tck dist modules and recurse into submodules
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- tools/version/src/main/java/ee/tck/versions/VersionRelease.java
+++ tools/version/src/main/java/ee/tck/versions/VersionRelease.java
@@ -18,6 +18,15 @@ import java.util.List;
 public class VersionRelease {
     // The x.y version of the platform
     private static final String TCK_BASE_VERSION = "11.0";
+    private static final HashSet<String> SKIPPED_MODULES = new HashSet<>();
+    static {
+        SKIPPED_MODULES.add("core-profile-tck");
+        SKIPPED_MODULES.add("expression-language-outside-container");
+        SKIPPED_MODULES.add("messaging-outside-container");
+        SKIPPED_MODULES.add("persistence-outside-container");
+        SKIPPED_MODULES.add("user_guides");
+    }
+
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
             System.out.println("Usage: VersionRelease <version>");
@@ -44,34 +53,8 @@ public class VersionRelease {
             document.write(writer);
         }
 
-        List<Element> modules = project.elements("modules");
-        modules.get(0).elements("module").forEach(module -> {
-            File moduleFile = new File(module.getText() + "/pom.xml");
-            try {
-                Document moduleDocument = reader.read(moduleFile);
-                Element moduleProject = moduleDocument.getRootElement();
-                String moduleVersion = getVersion(moduleProject);
-                String moduleGroupId = getGroupId(moduleProject);
-                String moduleArtifactId = moduleProject.element("artifactId").getText();
-                TestArtifact testArtifact = new TestArtifact(moduleGroupId, moduleArtifactId, moduleVersion, moduleFile);
-                artifacts.add(testArtifact);
-                System.out.printf("%s\n", testArtifact);
-                if(moduleVersion.startsWith(TCK_BASE_VERSION)) {
-                    System.out.println("Update version");
-                    moduleProject.element("version").setText(version);
-                    Element parentVersion = moduleProject.element("parent").element("version");
-                    if (parentVersion.getText().startsWith(TCK_BASE_VERSION)) {
-                        parentVersion.setText(version);
-                    }
-                }
-                // Write the updated module file
-                try(FileWriter writer = new FileWriter(moduleFile)) {
-                    moduleDocument.write(writer);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        // Update the submodule versions
+        updateModules(null, project, version);
 
         // Update the release module
         File releaseModule = new File("release/pom.xml");
@@ -86,6 +69,55 @@ public class VersionRelease {
         // Write a release script for deploying the artifacts
         writeReleaseScript(version);
         // Write a bom for the artifacts
+    }
+
+    void updateModules(File projectDir, Element project, String version) throws Exception {
+        SAXReader reader = new SAXReader();
+        List<Element> modules = project.elements("modules");
+        if(modules.isEmpty()) {
+            return;
+        }
+
+        for(Element module : modules.get(0).elements("module")) {
+            String modulePath = module.getText();
+            int lastSlash = modulePath.lastIndexOf('/');
+            String moduleName = modulePath.substring(lastSlash + 1);
+            if(SKIPPED_MODULES.contains(moduleName)) {
+                continue;
+            }
+            if(projectDir != null) {
+                modulePath = projectDir.getAbsolutePath() + "/" + modulePath;
+            }
+            File moduleFile = new File(modulePath + "/pom.xml");
+            try {
+                Document moduleDocument = reader.read(moduleFile);
+                Element moduleProject = moduleDocument.getRootElement();
+                String moduleVersion = getVersion(moduleProject);
+                String moduleGroupId = getGroupId(moduleProject);
+                String moduleArtifactId = moduleProject.element("artifactId").getText();
+                TestArtifact testArtifact = new TestArtifact(moduleGroupId, moduleArtifactId, moduleVersion, moduleFile);
+                artifacts.add(testArtifact);
+                System.out.printf("%s\n", testArtifact);
+                if(moduleVersion.startsWith(TCK_BASE_VERSION)) {
+                    setVersion(moduleProject, version);
+                    Element parentVersion = moduleProject.element("parent").element("version");
+                    if (parentVersion.getText().startsWith(TCK_BASE_VERSION)) {
+                        parentVersion.setText(version);
+                    }
+                }
+                // Write the updated module file
+                try(FileWriter writer = new FileWriter(moduleFile)) {
+                    moduleDocument.write(writer);
+                }
+
+                Element submodules = moduleProject.element("modules");
+                if(submodules != null) {
+                    updateModules(moduleFile.getParentFile(), moduleProject, version);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     void writeReleaseScript(String version) throws IOException {
@@ -105,6 +137,14 @@ public class VersionRelease {
     }
     String getVersion(Element root) {
         return getInfo(root, "version");
+    }
+    void setVersion(Element moduleProject, String version) {
+        Element versionElement = moduleProject.element("version");
+        if (versionElement != null) {
+            versionElement.setText(version);
+        } else {
+            moduleProject.element("parent").element("version").setText(version);
+        }
     }
     String getGroupId(Element root) {
         return getInfo(root, "groupId");
