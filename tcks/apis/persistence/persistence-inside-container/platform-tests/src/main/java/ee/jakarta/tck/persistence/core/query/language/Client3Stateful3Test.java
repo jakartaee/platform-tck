bbf8b766433ec8afadf6306007ab400c8faa6931
Scott M Stark <starksm@starkinternational.com>
2025-03-10 16:38:53 -0600

    Add missing AltWebVehicleRunner class Remove the fixed jndi name for stateless3 vehicle ejb
    Signed-off-by: Scott M Stark <starksm@starkinternational.com>

--- tcks/apis/persistence/persistence-inside-container/platform-tests/src/main/java/ee/jakarta/tck/persistence/core/query/language/Client3Stateful3Test.java
+++ tcks/apis/persistence/persistence-inside-container/platform-tests/src/main/java/ee/jakarta/tck/persistence/core/query/language/Client3Stateful3Test.java
@@ -71,6 +71,7 @@ public class Client3Stateful3Test extends ee.jakarta.tck.persistence.core.query.
             com.sun.ts.tests.common.vehicle.EmptyVehicleRunner.class,
             ee.jakarta.tck.persistence.common.PMClientBase.class,
             com.sun.ts.tests.common.vehicle.stateful3.Stateful3VehicleRunner.class,
+            com.sun.ts.tests.common.vehicle.web.AltWebVehicleRunner.class,
             ee.jakarta.tck.persistence.common.schema30.Util.class,
             com.sun.ts.tests.common.vehicle.VehicleRunnable.class,
             com.sun.ts.tests.common.vehicle.stateful3.Stateful3VehicleIF.class,
