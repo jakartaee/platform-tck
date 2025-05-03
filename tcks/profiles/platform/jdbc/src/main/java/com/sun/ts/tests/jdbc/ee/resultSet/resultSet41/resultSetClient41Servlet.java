d428d48dd7cb0f6de3f8edf71c81ac8e7059a0b1
Brian Decker <bmdecker@us.ibm.com>
2025-05-03 08:08:22 -0500

    Fix Stored Procedures for JDBC - Add missing 'web' tag

--- tcks/profiles/platform/jdbc/src/main/java/com/sun/ts/tests/jdbc/ee/resultSet/resultSet41/resultSetClient41Servlet.java
+++ tcks/profiles/platform/jdbc/src/main/java/com/sun/ts/tests/jdbc/ee/resultSet/resultSet41/resultSetClient41Servlet.java
@@ -50,6 +50,7 @@ import tck.arquillian.protocol.common.TargetVehicle;
  */
 
 @Tag("tck-javatest")
+@Tag("web")
 
 public class resultSetClient41Servlet extends resultSetClient41 implements Serializable {
 	private static final String testName = "jdbc.ee.resultSet.resultSet41";
