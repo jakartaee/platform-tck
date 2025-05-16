66de77669ab7906da8a4e9d50eed8c3f09e60f15
Scott M Stark <starksm@starkinternational.com>
2025-05-16 14:37:43 -0600

    Tests should not be using the maven resolver api
    Fixes #2314
    
    Signed-off-by: Scott M Stark <starksm@starkinternational.com>

--- tcks/apis/enterprise-beans/ejb32/src/test/java/LibResolveTest.java
+++ tcks/apis/enterprise-beans/ejb32/src/test/java/LibResolveTest.java
@@ -1,16 +1,22 @@
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenFormatStage;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenResolvedArtifact;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 
 public class LibResolveTest {
     @Test
     public void resolveCommonsLib() {
+        /* Obsolete, just add the classes to the archive
         String[] activeMavenProfiles = {"staging"};
         MavenFormatStage mavenFormatStage = Maven.resolver().loadPomFromFile("pom.xml", activeMavenProfiles)
                 .resolve("org.apache.commons:commons-lang3")
                 .withoutTransitivity();
         MavenResolvedArtifact resolvedArtifacts = mavenFormatStage.asSingleResolvedArtifact();
         System.out.println("Resolved artifact: " + resolvedArtifacts.asFile());
+
+         */
+        JavaArchive lib = ShrinkWrap.create(JavaArchive.class, "commons-lang3.jar");
+        lib.addClasses(org.apache.commons.lang3.StringUtils.class,
+                org.apache.commons.lang3.time.DateUtils.class);
+        System.out.println(lib.toString(true));
     }
 }
