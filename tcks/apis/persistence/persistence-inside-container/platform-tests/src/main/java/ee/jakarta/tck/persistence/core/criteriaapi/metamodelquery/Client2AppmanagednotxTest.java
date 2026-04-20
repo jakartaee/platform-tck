7c20010c38bea610fab4a58498add9ce337cc769
Scott Marlow <scott.marlow@ibm.com>
2026-04-20 15:17:55 -0400

    Platform TCK challenge 2614: For persistence.core.criteriaapi.metamodelquery address missed classloader failure in Client2*.java deployment where we needed to remove the Status.class like we did for Client1* + other tests in same package
    Signed-off-by: Scott Marlow <scott.marlow@ibm.com>

--- tcks/apis/persistence/persistence-inside-container/platform-tests/src/main/java/ee/jakarta/tck/persistence/core/criteriaapi/metamodelquery/Client2AppmanagednotxTest.java
+++ tcks/apis/persistence/persistence-inside-container/platform-tests/src/main/java/ee/jakarta/tck/persistence/core/criteriaapi/metamodelquery/Client2AppmanagednotxTest.java
@@ -78,7 +78,6 @@ public class Client2AppmanagednotxTest extends ee.jakarta.tck.persistence.core.c
             com.sun.ts.tests.common.vehicle.appmanagedNoTx.AppManagedNoTxVehicleRunner.class,
             com.sun.ts.tests.common.vehicle.web.AltWebVehicleRunner.class,
             ee.jakarta.tck.persistence.common.schema30.Util.class,
-            com.sun.ts.lib.harness.Status.class,
             com.sun.ts.tests.common.vehicle.VehicleRunnable.class,
             com.sun.ts.tests.common.vehicle.ejb3share.UserTransactionWrapper.class,
             EETest.class,
